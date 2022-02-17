08fb40587abe94eea7c2f5404748fca9b88a110d
Usman <usman.mughal@outlook.fr>
2022-02-17 16:11:57 +0100

    correction de la page product

--- front/js/product.js
+++ front/js/product.js
@@ -1,112 +1,145 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
 
   //-------------------fonction principale-------------------//
   //--------------------------------------------------------//
   async function main() {
 
-      // On Récupére l'Url.
-      const url = new URL(window.location.href);
-      // productId = à Id récupérer en paramètre de notre Url
-      let productId = url.searchParams.get("id");
+    // On Récupére l'Url.
+    const url = new URL(window.location.href);
+    // productId = à Id récupérer en paramètre de notre Url
+    let productId = url.searchParams.get("id");
 
-      // On Appel notre fonction qui va nous retourné notre produit de l'API
-      let product = await GetId(productId);
-    
-      // Fonction d'affichage du produit.
-      DisplayProduct(product);
-      console.log(product);
-  
+    // On Appel notre fonction qui va nous retourné notre produit de l'API
+    let product = await GetId(productId);
+
+    // Fonction d'affichage du produit.
+    DisplayProduct(product);
+
+    BtnClick(product);
   }
-  
+
   main();
 
-      //-------------------Fonction d'intérrogation de notre api avec productId-------------------//
-    //-----------------------------------------------------------------------------------------//
-    async function GetId(productId) {
-      return fetch("http://localhost:3000/api/products/"+productId)
+  //-------------------Fonction d'intérrogation de notre api avec productId-------------------//
+  //-----------------------------------------------------------------------------------------//
+  async function GetId(productId) {
+
+    return fetch("http://localhost:3000/api/products/" + productId)
       .then(function (response) {
-          //console.log(response);
-          if (response.ok) {
+        //console.log(response);
+        if (response.ok) {
           return response.json();
-          }
+        }
       })
       .catch(function (error) {
-          console.log(error);
+        console.log(error);
       });
+
   }
 
-    function DisplayProduct(product) {
+  function DisplayProduct(product) {
 
-      // Récuperation des parents
-      const parenttitle = document.getElementsByTagName("title")[0];
-      const parentImg = document.getElementsByClassName("item__img");
-      const parentName = document.getElementById("title");
-      const parentPrice = document.getElementById ("price");
-      const parentDescription = document.getElementById ("description");
-      const parentColor = document.getElementById("colors");
-      const parentQuantity = document.getElementById ("quantity");
-
-
-      var baliseimg = document.createElement("img");
-      baliseimg.setAttribute("src",product.imageUrl);
-      baliseimg.setAttribute("alt",product.altTxt);
-
-      parentImg[0].appendChild(baliseimg)
-
-      parenttitle.innerHTML = product.name;
-      parentName.innerText = product.name;
-      parentPrice.innerText = product.price;
-      parentDescription.innerText = product.description;
-      parentQuantity.innerText = product.quantity;
+    // Récuperation des parents
+    const parenttitle = document.getElementsByTagName("title")[0];
+    const parentImg = document.getElementsByClassName("item__img");
+    const parentName = document.getElementById("title");
+    const parentPrice = document.getElementById("price");
+    const parentDescription = document.getElementById("description");
+    const parentQuantity = document.getElementById("quantity");
 
 
+    var baliseimg = document.createElement("img");
+    baliseimg.setAttribute("src", product.imageUrl);
+    baliseimg.setAttribute("alt", product.altTxt);
 
- // Affichage des couleurs
-  let colorsList = product.colors;
+    parentImg[0].appendChild(baliseimg)
 
-  for (let color of colorsList) {
-    const displayColor = document.createElement("option");
-    displayColor.setAttribute("value", color);
-    displayColor.innerText = color;
-    parentColor.appendChild(displayColor);
+    parenttitle.innerHTML = product.name;
+    parentName.innerText = product.name;
+    parentPrice.innerText = product.price;
+    parentDescription.innerText = product.description;
+    parentQuantity.innerText = product.quantity;
+
+
+
+    //* Création des choix couleur-------------------------------------------------
+    let SelecteurCouleur = document.getElementById("colors")
+    let options = product.colors
+
+    options.forEach(function (element) {
+      SelecteurCouleur.appendChild(new Option(element, element));
+    })
+
   }
-}
 
-
- // Gestion du panier
-const parentColor = document.getElementById("colors");
-const parentQuantity = document.getElementById ("quantity");
-
-// je configure un eventListener quand l'utilisateur clique sur ajouter au panier
-const addToCart = document.getElementById('addToCart');
-addToCart.addEventListener('click', (event) => {
-  event.preventDefault();
-
-// Declaration de la classe produit
+  // Declaration de la classe produit
   class productClass {
-    constructor(id, name, color, qty, imgurl, price, alt) {
+    constructor(id, color, qty) {
       this.id = id;
-      this.name = name;
       this.color = color;
       this.qty = qty;
-      this.imgurl = imgurl;
-      this.price = price;
-      this.alt = alt;
     }
   }
 
-// Declaration d'une variable productInLocalStorage
-  let productInLocalStorage =  JSON.parse(localStorage.getItem('product'));
 
-// Ajout des produits selectionnés dans le local storage
-  const addProductLocalStorage = () => {
-    productInLocalStorage.push(productClass);
-    localStorage.setItem('product', JSON.stringify(productInLocalStorage));
+  function BtnClick(product) {
+    // Initialisation des variables.
+    let colorChoosen = "";
+    let qtyChoosen = "";
+    let qty = "";
+    let BtnPanier = document.getElementById("addToCart");
+
+    // Sélection des couleurs avec sont comportement au change.
+    let colorSelection = document.getElementById("colors");
+    colorSelection.addEventListener("change", function (e) {
+      colorChoosen = e.target.value;
+      console.log(colorChoosen);
+    });
+
+    let qtySelection = document.getElementById("quantity");
+    qtySelection.addEventListener("change", function (e) {
+      qty = e.target.value;
+      console.log(qtyChoosen);
+    });
+
+    BtnPanier.addEventListener("click", function () {
+
+
+      // Initialisation variable
+      let ProductLocalStorage = [];
+      let oldQty = 0;
+
+      for (let i = 0; i < localStorage.length; i++) {
+        ProductLocalStorage[i] = JSON.parse(localStorage.getItem(localStorage.key(i)));
+
+        if (product._id === ProductLocalStorage[i].id && ProductLocalStorage[i].color === colorChoosen) {
+          oldQty = ProductLocalStorage[i].qty;
+        }
+      }
+
+      qtyChoosen = parseInt(oldQty) + parseInt(qty);
+
+      let productChoosen = new productClass(
+        product._id,
+        colorChoosen,
+        qtyChoosen
+      )
+
+      if (colorChoosen != "" && qtyChoosen >= 1 && qtyChoosen <= 100) {
+
+        localStorage.setItem(
+          product.name + " " + colorChoosen,
+          JSON.stringify(productChoosen)
+        )
+      } else {
+        alert("tu t'est tromper ma caille");
+      }
+
+
+    })
+
+
+
   }
-
-
-
-
-});
 
 });
